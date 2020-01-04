3dfb2bd2f0153b24d34885ebde7de550f3cf5fa1
Robert Kazirut <rakazirut@oakland.edu>
2020-01-04 14:00:27 -0500

    +Add Bar Chart Material

---
+++ Course/PlotlyLearn/BarChart/BarChartExercise/BarChartExercise.py
@@ -0,0 +1,11 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('mocksurvey.csv', index_col=0)
+# Horizontal stacked bar char
+data = [go.Bar(y=df.index, x=df[response], orientation='h', name=response) for response in df.columns]
+
+layout = go.Layout(title='Survey Results', barmode='stack')
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='barchartexercise.html')
--- Course/PlotlyLearn/LineChart/LineChartExercise/LineChartExercise.py
+++ Course/PlotlyLearn/LineChart/LineChartExercise/LineChartExercise.py
@@ -6,13 +6,22 @@ df = pd.read_csv('2010YumaAz.csv')
 days = ['TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY', 'MONDAY']
 
 data = []
-
+# Fahrenheit
 for day in days:
     trace = go.Scatter(x=df['LST_TIME'],
-                       y=df[df['DAY'] == day]['T_HR_AVG'],
+                       y=(df[df['DAY'] == day]['T_HR_AVG'] * (9 / 5)) + 32,
+                       # y=df[df['DAY'] == day]['T_HR_AVG'],
                        mode='lines',
                        name=day)
     data.append(trace)
+
+# Celsius
+# for day in days:
+#     trace = go.Scatter(x=df['LST_TIME'],
+#                         y=df[df['DAY'] == day]['T_HR_AVG'],
+#                         mode='lines',
+#                         name=day)
+#     data.append(trace)
 
 # The below is a single line declaration of data which is slightly harder to read but yields the same result
 # data = [{
@@ -23,4 +32,6 @@ for day in days:
 
 layout = go.Layout(title='Daily Temp Average')
 fig = go.Figure(data=data, layout=layout)
+fig.layout.paper_bgcolor = '#000'
+fig.layout.plot_bgcolor = '#000'
 pyo.plot(fig, filename='linechartexercise.html')
